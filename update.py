c786bb3261cd4031737783abb5c50e8c75bebd7b
lobnaabdelhamed97git config --global user.name lobnaabdelhamed97git config --global user.name lobnaabdelhamed97 <lola_2014@hotmail.com>
2021-08-09 11:42:27 +0200

    new update

--- update.py
+++ update.py
@@ -2,15 +2,26 @@ from flask import request
 from flask.json import jsonify
 from flask_mysqldb import MySQL
 from config import app
+import pymysql
+from pymysql import connect,Error
 # Connect to the MySQL Database Server
 def connect ():
-    mysql = MySQL(app)
-    con = mysql.connection
-    return con
+    try:
+        with pymysql.connect(
+            host="127.0.0.1",
+            user="root",            
+            password='',
+            database="postett_local",
+    ) as connection:
+            return connection
+    except Error as e:
+        return e
+    #mysql = MySQL(app)
+    #con = mysql.connection
+    #return con
 @app.route('/', methods=['POST'])
 def UpdateUser():
         con=connect()
-        cur=con.cursor()
         user = request.get_json()
         lastkey=list(user.keys())[-1]
         query="UPDATE users SET "
@@ -22,10 +33,15 @@ def UpdateUser():
                 else:
                     query=query+key+" = "+"'"+user[key]+"'"+','
         query=query+" WHERE id = "+"'"+user['id']+"'"+";"    
-        
+        cur=con.cursor()
+        print(query)
+        #try:
         cur.execute(query)
-        con.commit()
         cur.close()
-        return jsonify('success') 
+        con.close()
+        return jsonify('success')
+        #except Exception:
+        cur.close()
+        return jsonify('Error: unable to update items')        
 if __name__ == '__main__':
     app.run()
